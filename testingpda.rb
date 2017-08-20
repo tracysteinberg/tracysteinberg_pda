33f8ec99e2c7effe84aed5fabc1cc49274c282b3
Tracy Steinberg <tracyadelesteinberg@gmail.com>
2017-08-20 19:29:00 +0100

    pda work 20/08/17

---
+++ testingpda.rb
@@ -0,0 +1,16 @@
+
+# sorting
+numbers = [7, 5, 3, 10, 15, 16, 24 ,19]
+numbers = numbers.sort.reverse!
+puts numbers
+
+# searching
+numbers = [7, 5, 3, 10, 15, 16, 24 ,19]
+puts numbers.first
+
+
+
+
+
+
+
